17e0a707cfcdf6f2b7d804c67698b856a17b5826
Erkebulan <71064146+Erkebnulan@users.noreply.github.com>
2021-03-14 14:56:06 +0600

    Signed-off-by: Erkebulan <71064146+Erkebnulan@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -104,3 +104,11 @@ $('.aside-close').click(function (){
     }
 })
 
+//change mode
+$('.switch-dark input').change(function (){
+    if($(this).is(':checked')){
+        $('body').addClass('dark-mode');
+    } else {
+        $('body').removeClass('dark-mode');
+    }
+})
